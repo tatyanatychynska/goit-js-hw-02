e0c1b78260f5dff88fe4165a98fbad0a0d9250bf
Tanya Tychynska <tatyanazyeva@gmail.com>
2025-11-30 14:12:09 +0100

    HW02 JS

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,15 @@
-function getElementWidth(content, padding, border) {
-    const numContent = Number.parseFloat(content);
-    const numPadding = Number.parseFloat(padding);
-    const numBorder = Number.parseFloat(border);
-    return numContent + numPadding * 2 + numBorder * 2;
+function checkForSpam(message) {
+    let lowMessage = message.toLowerCase();
+    if (lowMessage.includes("spam") || lowMessage.includes("sale")) {
+        return true;
+    }
+    return false;
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
